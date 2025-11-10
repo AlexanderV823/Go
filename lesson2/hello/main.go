e07201c6dfd7e8bb8806e5d5ec0279ca32f28954
AlexanderV823 <volodin_aa@hotmail.com>
2025-11-11 00:31:53 +0300

    Добавлен исходный файл примера из Урока 2

--- lesson2/hello/main.go
+++ lesson2/hello/main.go
@@ -7,4 +7,12 @@ import (
 func main() {
 	fmt.Println("Hello, World!") //Непосредственно вывод сообщения
 	fmt.Println("Hello, Sanek!")
+	a := 1
+	b := 0
+	if b == 0 {
+		fmt.Println("Division by zero!")
+		return
+	}
+	fmt.Println(a / b)
+
 }
