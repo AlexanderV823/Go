83138833d3d591ea1268acf84b5a74449dee818c
AlexanderV823 <volodin_aa@hotmail.com>
2025-11-10 23:49:29 +0300

    Добавлен пример из Урока 2

---
+++ lesson2/hello/main.go
@@ -0,0 +1,10 @@
+package main //Объявляем главный пакет
+
+import (
+	"fmt" //стандартный пакет для вывода текста
+)
+
+func main() {
+	fmt.Println("Hello, World!") //Непосредственно вывод сообщения
+	fmt.Println("Hello, Sanek!")
+}
