c2715827cb7e8313c58285110a28dfe09f032867
AlexanderV823 <volodin_aa@hotmail.com>
2025-11-24 20:45:17 +0300

    Сохранение решения ДЗ 6

---
+++ lesson6/hw_6/addEmp/addEmp.go
@@ -0,0 +1,35 @@
+package addEmp
+
+import (
+	"fmt"
+	"github.com/AlexanderV823/Go/tree/main/lesson6/hw_6/findEmp"
+)
+
+// Добавляет структуру сотрудника в срез. Если сотрудник с таким ID уже есть в срезе, данные о нем перезаписываются.
+// emp - структура с данными сотрудника.
+// arr - срез для поиска и записи.
+func AddEmployee(emp Employee, slc []Employees) (err error) {
+
+	if err != nil {
+		return fmt.Errorf("Ошибка вызова addEmployee: %w", err)
+	}
+
+	// Сначала, нужно проверить нет ли уже такого сотрудника
+	// Ищем сотрудника по ID
+	findIndex, err := findEmp.FindEmployee(slc, emp.ID)
+
+	if err != nil {
+		return fmt.Errorf("Ошибка поиска: %w", err)
+	}
+
+	if findIndex != -1 {
+		// Если сотрудник найден, то обновляем данные
+		slc[findIndex] = emp
+	} else {
+		// Если сотрудника с таким ID нет, то добавляем нового
+		slc = append(slc, emp)
+	}
+
+	return nil
+
+}
---
+++ lesson6/hw_6/findEmp/findEmp.go
@@ -0,0 +1,22 @@
+package findEmp
+
+import (
+	"fmt"
+)
+
+// Производит поиск сотрудника по ID в переданном срезе или массиве.
+// array - срез для поиска,
+// id - id-сотрудника.
+// Возврашает индекс записи о сотруднике или -1 и error.
+func FindEmployee(slice []byte, id int) (int, error) {
+
+	for i := range slice {
+		emp := slice[i]
+		if emp.ID == id {
+			return i, nil
+		}
+	}
+
+return -1, fmt.Errorf("сотрудник не найден")
+
+}
