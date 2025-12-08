daac19553f6e04eb6243ab3c967a77625f460206
AlexanderV823 <volodin_aa@hotmail.com>
2025-12-08 22:15:10 +0300

    В пакет storage добавлена общая функция MakePath, для получения пути к файлу

---
+++ GoBasics/project_work/todo-app/internal/storage/make_path.go
@@ -0,0 +1,30 @@
+package storage
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// MakePath - кроссплатформенно формирует путь для сохранения файла относительно домашнего каталога пользователя.
+// fileName - имя файла
+func MakePath(fileName string) (filePath string, err error) {
+
+	// Находим домашний каталог
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("ошибка создания файла: %w", err)
+	}
+
+	// Объединяем путь к домашнему каталогу и имя папки
+	folderPath := filepath.Join(homeDir, "MyTasks")
+
+	// Создаем папку, если она не существует.
+	err = os.MkdirAll(folderPath, 0644)
+	if err != nil {
+		return "", fmt.Errorf("ошибка создания файла: %w", err)
+	}
+
+	// Возвращаем созданный файл
+	return filepath.Join(folderPath, fileName), nil
+}
