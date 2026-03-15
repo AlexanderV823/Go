3ec5fb121fbe6a0af683cdec20b9aff60f1af6ae
AlexanderV823 <volodin_aa@hotmail.com>
2026-03-15 21:21:04 +0300

    Реализована функция readLogs

--- GoMult/project_work/log-processor/main.go
+++ GoMult/project_work/log-processor/main.go
@@ -2,13 +2,53 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
-	"processor"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
-func main() {
+type LogEntry struct {
+	Timestamp    string // время в формате "2024-01-15 10:30:00"
+	IP           string // IP адрес клиента
+	Method       string // HTTP метод (GET, POST и т.д.)
+	URL          string // путь запроса
+	StatusCode   int    // HTTP статус код
+	ResponseTime int    // время ответа в миллисекундах
+}
 
+type Statistics struct {
+	TotalRequests   int            // общее количество запросов
+	ErrorCount      int            // количество ошибок (статус >= 400)
+	RequestsByIP    map[string]int // количество запросов с каждого IP
+	AverageRespTime float64        // среднее время ответа
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	const filename string = "./log-processor/testdata/log.csv"
+	const numWorkers int = 3
+	var wg sync.WaitGroup
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM)
+	
+	go func() {
+		<-sigCh
+		cancel()
+	}()
+
+	// Получаем канал в который пишутся строки лога
+	entryCh, err := readLogs(ctx, filename)
+	if err != nil {
+		log.Printf("error read CSV: %v", err)
+	}
+
+	// Передаем канал со строками лога в worker pool
+	processedCh := processLogs(ctx, entryCh, numWorkers)
 }
--- GoMult/project_work/log-processor/processor.go
+++ GoMult/project_work/log-processor/processor.go
@@ -2,35 +2,104 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
 	"sync"
 )
 
 // readLogs - выполняет чтение логов из файлов
-func readLogs() {
+func readLogs(ctx context.Context, filename string) (<-chan LogEntry, error) {
+	outCh := make(chan LogEntry)
 
+	go func() {
+		defer close(outCh)
+
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Printf("open CSV file error: %v", err)
+			return
+		}
+		defer file.Close()
+
+		reader := csv.NewReader(file)
+		_, _ = reader.Read() // Skip top line
+
+		for {
+			select {
+			case <-ctx.Done():
+				log.Printf("context cancel: %v", ctx.Err())
+				return
+			default:
+				line, err := reader.Read()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Printf("string read error: %v", err)
+					continue
+				}
+
+				logEntry, err := parseLogLine(line)
+				if err != nil {
+					log.Printf("pass line: %v", err)
+					continue
+				}
+
+				select {
+				case <-ctx.Done():
+					return
+				case outCh <- logEntry:
+				}
+			}
+		}
+	}()
+	return outCh, nil
 }
 
-// processLogs - обрабатывает логи
-func processLogs() {
-
+// processLogs - обрабатывает логи через worker pool
+func processLogs(ctx context.Context, input <-chan LogEntry, numWorkers int) <-chan LogEntry {
+	
 }
 
 // filterLogs - фильтрует записи по статус-коду
-func filterLogs() {
+func filterLogs(input <-chan LogEntry, minStatus int) <-chan LogEntry {
 
 }
 
 // calculateStats - выполняет подсчет статистики
-func calculateStats() {
+func calculateStats(input <-chan LogEntry) Statistics {
 
 }
 
-// parseLogLine - выполняет парсинг строки CSV в структуру LogEntry
-func parseLogLine(line string) (LogEntry, error) {
+// parseLogLine - выполняет чтение строки CSV в структуру LogEntry
+func parseLogLine(line []string) (LogEntry, error) {
 
+	if len(line) < 6 {
+		return LogEntry{}, fmt.Errorf("not enough data in the row: %v", line)
+	}
+
+	statusCode, err := strconv.Atoi(line[4])
+	if err != nil {
+		return LogEntry{}, fmt.Errorf("status code convert error: %v", err)
+	}
+
+	responseTime, err := strconv.Atoi(line[5])
+	if err != nil {
+		return LogEntry{}, fmt.Errorf("response time convert error: %v", err)
+	}
+
+	return LogEntry{
+			Timestamp:    line[0],
+			IP:           line[1],
+			Method:       line[2],
+			URL:          line[3],
+			StatusCode:   statusCode,
+			ResponseTime: responseTime},
+		nil
 }
 
 // printTopIPs - выводит топ IP-адресов
