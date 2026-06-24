c27b8c402a1c3c9ecfe48366107b027438c7fa62
AlexanderV823 <volodin_aa@hotmail.com>
2026-06-24 19:39:56 +0300

    Добавлена точка входа

---
+++ GoPro/hw_1/cmd/order-api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"hw_1/internal/infrastructure/db"
+	"hw_1/internal/infrastructure/notification"
+	"hw_1/internal/service"
+)
+
+func main() {
+	database, err := sql.Open("sqlite3", "orders.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	// 1. Инициализация и сборка инфраструктурных адаптеров
+	sqliteRepo := db.NewSQLiteRepository(database)
+	if err := sqliteRepo.InitSchema(); err != nil {
+		log.Fatal(err)
+	}
+
+	notifier := notification.NewEmailService()
+
+	// 2. Внедрение зависимостей в слой бизнес-логики (DIP)
+	orderService := service.NewOrderService(sqliteRepo, notifier)
+
+	// 3. Запуск бизнес-процесса
+	err = orderService.CreateOrder("Иван", []string{"apple", "banana"}, 10.5)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
