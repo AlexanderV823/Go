45233c39f5a226801607e5f8c21e9dac0ef4727a
AlexanderV823 <volodin_aa@hotmail.com>
2026-08-06 20:01:23 +0300

    Добавлен эндпоинт /health

--- GoPro/hw_3/tasks-api/cmd/server/main.go
+++ GoPro/hw_3/tasks-api/cmd/server/main.go
@@ -9,12 +9,16 @@ import (
 )
 
 func main() {
-	// Подключаем in-memory реализацию хранилища
+	// Подключаем потокобезопасную in-memory реализацию интерфейса Storage
 	store := memory.New()
 
 	h := handlers.New(store)
 
 	mux := http.NewServeMux()
+
+	// РЕГИСТРАЦИЯ ЗДЕСЬ: Добавляем маршрут для проверки здоровья
+	mux.HandleFunc("/health", h.Health)
+
 	mux.HandleFunc("/tasks", h.TasksCollection) // GET, POST
 	mux.HandleFunc("/tasks/", h.TaskItem)       // GET, PUT, DELETE
 
