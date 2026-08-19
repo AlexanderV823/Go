38ca323323cfc71629600115768bc8ae61790b84
AlexanderV823 <volodin_aa@hotmail.com>
2026-08-19 20:21:04 +0300

    feat: реализован пакет auth

--- GoPro/final/blog-api/pkg/auth/password.go
+++ GoPro/final/blog-api/pkg/auth/password.go
@@ -1,63 +1,106 @@
 package auth
 
 import (
+	"crypto/rand"
 	"errors"
+	"math/big"
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
-	ErrEmptyPassword    = errors.New("password cannot be empty")
-	ErrPasswordTooShort = errors.New("password is too short")
+	ErrEmptyPassword         = errors.New("password cannot be empty")
+	ErrPasswordTooShort      = errors.New("password is too short")
+	ErrNoUppercase           = errors.New("password must contain at least one uppercase letter")
+	ErrNoLowercase           = errors.New("password must contain at least one lowercase letter")
+	ErrNoDigit               = errors.New("password must contain at least one digit")
 )
 
 // HashPassword хеширует пароль используя bcrypt
 func HashPassword(password string) (string, error) {
-	// TODO: Реализовать хеширование пароля
-	// Шаги:
-	// 1. Проверить что пароль не пустой
-	// 2. Использовать bcrypt для хеширования
-	// 3. Выбрать подходящий cost factor (например, 10-12)
-	// 4. Вернуть хешированный пароль как строку
-	//
-	// Подсказка: используйте golang.org/x/crypto/bcrypt
+	if len(password) == 0 {
+		return "", ErrEmptyPassword
+	}
 
-	return "", errors.New("not implemented")
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
 }
 
 // CheckPassword проверяет соответствие пароля и его хеша
 func CheckPassword(password, hash string) bool {
-	// TODO: Реализовать проверку пароля
-	// Шаги:
-	// 1. Сравнить пароль с хешом используя bcrypt
-	// 2. Вернуть true если пароль совпадает, false если нет
-	// 3. При ошибке вернуть false
-	//
-	// Подсказка: bcrypt.CompareHashAndPassword
-
-	return false
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 // ValidatePasswordStrength проверяет надежность пароля
 func ValidatePasswordStrength(password string) error {
-	// TODO: Реализовать проверку надежности пароля
-	// Требования:
-	// - Минимум 6 символов
-	// - Опционально: содержит буквы и цифры
-	// - Опционально: содержит заглавные и строчные буквы
-	//
-	// Вернуть соответствующую ошибку или nil
+	// Проверка на пустоту
+	if len(password) == 0 {
+		return ErrEmptyPassword
+	}
 
-	return errors.New("not implemented")
+	// Требование: Минимум 6 символов
+	if len(password) < 6 {
+		return ErrPasswordTooShort
+	}
+
+	var (
+		hasUpper bool
+		hasLower bool
+		hasDigit bool
+	)
+
+	// Перебираем каждый символ пароля (как rune для поддержки UTF-8)
+	for _, r := range password {
+		switch {
+		case unicode.IsUpper(r):
+			hasUpper = true
+		case unicode.IsLower(r):
+			hasLower = true
+		case unicode.IsDigit(r):
+			hasDigit = true
+		}
+	}
+
+	// Проверка наличия заглавных букв
+	if !hasUpper {
+		return ErrNoUppercase
+	}
+
+	// Проверка наличия строчных букв
+	if !hasLower {
+		return ErrNoLowercase
+	}
+
+	// Проверка наличия цифр
+	if !hasDigit {
+		return ErrNoDigit
+	}
+
+	return nil
 }
 
-// GenerateRandomPassword генерирует случайный пароль (опциональное задание)
+// GenerateRandomPassword генерирует случайный пароль
 func GenerateRandomPassword(length int) (string, error) {
-	// TODO: Реализовать генерацию случайного пароля
-	// Шаги:
-	// 1. Создать набор допустимых символов
-	// 2. Сгенерировать случайную последовательность заданной длины
-	// 3. Вернуть пароль как строку
-	//
-	// Подсказка: используйте crypto/rand для криптографически стойкой генерации
+	if length <= 0 {
+		return "", errors.New("length must be greater than zero")
+	}
 
-	return "", errors.New("not implemented")
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+"
+
+	result := make([]byte, length)
+	for i := range result {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		result[i] = charset[num.Int64()]
+	}
+
+	return string(result), nil
 }
